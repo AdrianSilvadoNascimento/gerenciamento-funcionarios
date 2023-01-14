8206940bfa08aea8c4c260922848c73cac53c2da
Adrian Silva <adrianfsf10@gmail.com>
2023-01-14 10:24:31 -0300

    Adiciona MainBody

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
 import { useState } from 'react'
 import { Header } from './components/main/header/header'
+import MainBody from './components/main/main/main-body'
 import './App.scss'
 
 export default function App() {
-  let [name, setName] = useState('')
-
-  function getNameValue(props: any) {
-    setName(props.target.value)
-  }
-
   return (
-    <section>
-      <Header/>
-      {/* <h2>Olá</h2>
-      <form>
-        <input 
-          type="text" 
-          placeholder="Preencher com o nome" 
-          onChange={getNameValue}/>
-          <button>Get Name</button>
-        <div>
-          <h2>Hello, {name}!</h2>
-        </div>
-      </form> */}
-    </section>
+    <>
+      <section>
+        <Header />
+      </section>
+      <div>
+        <MainBody />
+      </div>
+    </>
   )
 }
---
+++ src/components/main/main/main-body.tsx
@@ -0,0 +1,7 @@
+export default function MainBody() {
+  return (
+    <div>
+      <h2>Olá MainBody</h2>
+    </div>
+  )
+}
